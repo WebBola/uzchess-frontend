dcab0c9522b5b3873fa0bc72ab4de4f544a41eff
WebBola <webdasturchi2009@gmail.com>
2026-05-11 18:25:30 +0500

    v2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import TopBooks from './top-books'
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <div>
+    <App />
+    <br />
+    <TopBooks />
+  </div>
 )
